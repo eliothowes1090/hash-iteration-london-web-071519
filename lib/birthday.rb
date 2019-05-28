3090907a3a54a61e0b288ac4d68d717c36c0e579
Eliot Howes <ejhowes@gmail.com>
2019-05-28 06:00:51 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -15,6 +15,8 @@ def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |key, value|
     if value < 13
       puts "Happy Birthday #{key}! You are now #{value} years old!"
+    else
+      puts "You are too old for this"
     end
   end
 end
